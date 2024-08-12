03a096a6e70e3fcb55defa755288d6f5996d0ab1
AndrewCase <49734742+Casemaster2000@users.noreply.github.com>
2024-08-12 16:44:18 +1000

    swagger-ui and API docs completed

--- habit-tracker/src/main/java/com/example/habittracker/HabitTrackerApplication.java
+++ habit-tracker/src/main/java/com/example/habittracker/HabitTrackerApplication.java
@@ -3,6 +3,7 @@ package com.example.habittracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class HabitTrackerApplication {
 
