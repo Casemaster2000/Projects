c468e09655d4677245413cc45ee7b201388f40c0
AndrewCase <49734742+Casemaster2000@users.noreply.github.com>
2025-04-27 18:21:50 +1000

    Delete habit-tracker directory

--- habit-tracker/src/main/java/com/example/habittracker/exception/HabitNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.habittracker.exception;
-
-public class HabitNotFoundException extends RuntimeException {
-    public HabitNotFoundException(Long id) {
-        super("Habit not found with id: " + id);
-    }
-}
